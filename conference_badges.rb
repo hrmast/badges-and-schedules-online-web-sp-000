bcebe3a12f921d71f6404a5a0ffe27e2e7be27a4
Heath Mast <hrmast@gmail.com>
2020-04-08 04:04:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,16 +8,18 @@ end
 def batch_badge_creator(name)
   arr = []
   name.each {|name| arr << badge_maker(name)}
-  arr.inspect
+  arr
 end
 
 def assign_rooms(name)
   assign = []
   name.each_with_index {|name, index| assign << ("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
-  assign.inspect
+  assign
 end
 
 def printer(name)
-batch_badge_creator(name)
-assign_rooms(name)
+badge = []
+name.each {|name| badge << batch_badge_creator(name)}
+name.each {|name| badge << assign_rooms(name)}
+puts badge.inspect
 end
