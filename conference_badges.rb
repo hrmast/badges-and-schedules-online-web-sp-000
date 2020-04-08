77b037e67b70dd664267df76ff5140a3fde376f9
Heath Mast <hrmast@gmail.com>
2020-04-08 03:05:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  arr = []
+  name.each {|name| arr << badge_maker(name)}
+  arr
+end
+
+def assign_rooms(name)
+  assign = []
+  name.each_with_index {|name, index| assign << ("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
+  return assign
+end
+
+def batch_badge_creator
+
+end
+
+def printer(name)
+  print badge_maker
+  print assign_rooms
+end
